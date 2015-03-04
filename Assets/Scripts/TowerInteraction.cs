a9f470e847c30699eb9a1bd811c65337d285dd20
tommy <tommysays.hi@hotmail.com>
2015-03-04 09:29:06 -0500

    Forgot to put a few things in init commit.

---
+++ Assets/Scripts/TowerInteraction.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerInteraction : MonoBehaviour {
+	public GameObject selectionIndicator;
+	public bool isSelected = false;
+
+	void Start () {
+		UpdateSelectionIndicatorColor();
+	}
+
+	void OnMouseUp(){
+		print ("isSelected = " + isSelected);
+		isSelected = !isSelected;
+		UpdateSelectionIndicatorColor();
+	}
+	// Update is called once per frame
+	void UpdateSelectionIndicatorColor() {
+		float alpha = isSelected ? 0.0f : 1.0f;
+		Color last = selectionIndicator.renderer.material.color;
+		Color next = new Color(last.r, last.g, last.b, alpha);
+		selectionIndicator.renderer.material.SetColor("_Color", next);
+	}
+}
---
+++ Assets/Scripts/Vida.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vida : MonoBehaviour {
+	public int curHP = 1, maxHP = 1;
+	public int damage = 1;
+	public float speed;
+	public Owner owner;
+	public enum Owner{
+		FRIENDLY, ENEMY, NEUTRAL
+	}
+	void Start(){
+		curHP = maxHP;
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Boundary" || other.tag == "Tower")
+			return;
+		GameObject curObj = GetComponent<Collider>().gameObject;
+		GameObject otherObj = other.gameObject;
+		Vida otherVida = otherObj.GetComponent<Vida>();
+		if (otherVida == null || owner == otherVida.owner){
+			return;
+		}
+		bool otherDestroyed = DamageObject(otherObj, damage);
+		bool thisDestroyed = DamageObject(curObj, otherVida.damage);
+		if (otherDestroyed && thisDestroyed){
+			// this is here to remove warnings lol. I'll use them for real later.
+		}
+		/* 
+		 * Code for spawning explosions.
+		if (otherDestroyed && other.tag == "Enemy"){
+			GameObject exp = Instantiate(other.GetComponent<EnemyController>().explosion, other.transform.position, Quaternion.identity) as GameObject;
+			Destroy(exp, 5);
+		} else if (thisDestroyed && this.tag == "Enemy"){
+			GameObject exp = Instantiate(GetComponent<EnemyController>().explosion, transform.position, Quaternion.identity) as GameObject;
+			Destroy(exp, 5);
+			gameController.increaseScore(100);
+		}
+		 */
+	}
+
+	bool DamageObject(GameObject obj, int damage){
+		Vida vida = obj.GetComponent<Vida>();
+		vida.curHP -= damage;
+		if (vida.curHP < 0)
+			vida.curHP = 0;
+		if (vida.curHP == 0){
+			Destroy (obj);
+			return true;
+		}
+		return false;
+	}
+}
